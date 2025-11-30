5111ee73273707a494efa1a922fa16a5492d590c
Румский Александр <arumskiy@gmail.com>
2025-11-30 23:22:09 +0300

    wip

--- main.py
+++ main.py
@@ -5,3 +5,5 @@ print ("This is hello from EL-1 Task")
 print ("This is sum function from EL-2 Task")
 
 print ("This is sum function from EL-3 Task")
+
+print ("This is parser function from EL-4 Task")
